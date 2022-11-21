8ef33c2b895c37229b86eb124ff24510c5bbfdd1
Sonja Peschke <sonjape91@gmail.com>
2022-11-21 12:00:05 +0100

    Included link to GitHub repo and Netlify

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ function App() {
           <h1>Dictionary</h1>
         </header>
         <main><Dictionary defaultKeyword="lettuce"/></main>
-        <footer className="App-footer"><small>Coded by Sonja</small></footer>
+        <footer className="App-footer">
+          <small>
+            This project was coded by Sonja and is{" "}
+            <a href="https://github.com/code-kedi/dictionary-app-react" target="_blank" rel="noreferrer">open-sourced on Github</a>
+            {" "}and{" "}<a href="https://playful-donut-fbc49a.netlify.app/" target="_blank" rel="noreferrer"> hosted on Netlify</a>
+          </small>
+        </footer>
       </div>
     </div>
   );
